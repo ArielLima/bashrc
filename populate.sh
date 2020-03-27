7b821c383dcbc5c31aca4a1982ad06646b62621e
Ari Lima <arilima@localhost.localdomain>
2020-03-27 12:12:43 -0400

    rename files

--- populate.sh
+++
@@ -1,7 +0,0 @@
-# Script for creating and populating current .bashrc file with the one located in this repo
-
-# make sure .bashrc exists
-touch ~/.bashrc
-
-# populate .bashrc
-cat bashrc > ~/.bashrc
